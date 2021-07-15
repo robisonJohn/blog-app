5293811015a9e07d18e156f45ffb14d2d9c23d10
johnrobison <robisonj8680@gmail.com>
2021-07-15 15:59:23 -0400

    built out the api configuration

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+let apiUrl;
+
+const apiUrls = {
+    production: 'https://coolest-kids-on-the-blog.herokuapp.com/api/posts',
+    development: 'http://localhost:3000/api'
+}
+
+if (window.location.hostname === 'localhost') {
+    apiUrl = apiUrls.development
+} else {
+    apiUrl = apiUrls.production
+}
+
+const api = axios.create({
+    baseURL: apiUrl
+})
+
+export default api;
