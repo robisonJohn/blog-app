229036283d5f13e8cfea57ae72dffbf9c80c7e63
ashleymhuynh <ashley.huynh23@gmail.com>
2021-07-15 13:17:02 -0700

    add navbarfooter, layout and serarch bar -- rendered navbar on page for testing

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h4>Created By</h4>
+      <h5>John Robinson, Andrea Llano, Lavanya Prabakar, and Ashley Huynh</h5>
+    </div>
+  );
+}
+
+export default Footer;
