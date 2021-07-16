55ffaf3c70f6bb0a1627362897b2d608e436d4de
johnrobison <robisonj8680@gmail.com>
2021-07-16 09:44:30 -0400

    added PostCreate.jsx and PostCreate.css

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -22,5 +22,48 @@ const PostCreate = (props) => {
         })
     }
 
-    // start handle submit tomorrow
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const created = await createPost(post)
+        setCreated({ created })
+    }
+
+    if (isCreated) {
+        return <Redirect to={`/posts`}/>
+    }
+
+    return (
+        <Layout>
+            <form className="create-form" onSubmit={handleSubmit}>
+                <input 
+                className="input-title"
+                placeholder="Title"
+                value={post.title}
+                name='title'
+                required
+                onChange={handleChange}
+                />
+
+                <textarea 
+                className="text-area description"
+                rows={10}
+                placeholder='Description'
+                value={post.description}
+                name='description'
+                required
+                onChange={handleChange}
+                />
+                <input 
+                className='input-image-link'
+                placeholder='Image Link'
+                value={post.imgURL}
+                name='imgURL'
+                required
+                onChange={handleChange}
+                />
+                <button type="submit" className="submit-button">Submit</button>
+                
+            </form>
+        </Layout>
+    )
 }
