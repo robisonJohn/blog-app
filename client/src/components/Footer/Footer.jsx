734ea504899d4d037996701c76dfcc070103c075
ashleymhuynh <ashley.huynh23@gmail.com>
2021-07-16 13:41:07 -0700

    Fixed John's name

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="Footer">
       <h4>Created By</h4>
-      <h5>John Robinson, Andrea Llano, and Ashley Huynh</h5>
+      <h5>John Robison, Andrea Llano, and Ashley Huynh</h5>
     </div>
   );
 }
